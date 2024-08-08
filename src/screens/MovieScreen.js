aabdbac37c90e1f7b744e32be7e3e7016fefae5c
Justin Kurdila <justinkurdila@gmail.com>
2024-08-07 20:29:01 -0400

    adding more recommendations

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-catch-shadow */
 import React, {useState, useEffect} from 'react';
 import {
   View,
@@ -12,7 +14,8 @@ import {
 } from 'react-native';
 
 function MoviesScreen() {
-  const [movies, setMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
   const [myList, setMyList] = useState([]);
   const [likedList, setLikedList] = useState([]);
   const [watchedList, setWatchedList] = useState([]);
@@ -22,15 +25,14 @@ function MoviesScreen() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  // Fetch movies from the API
   useEffect(() => {
-    const fetchMovies = async () => {
+    const fetchPopularMovies = async () => {
       try {
         const response = await fetch(
           'https://api.themoviedb.org/3/movie/popular?api_key=15979629ea6e558ef491c9b9ccee0043',
         );
         const data = await response.json();
-        setMovies(data.results);
+        setPopularMovies(data.results);
       } catch (error) {
         setError(error);
       } finally {
@@ -38,7 +40,25 @@ function MoviesScreen() {
       }
     };
 
-    fetchMovies();
+    fetchPopularMovies();
+  }, []);
+
+  useEffect(() => {
+    const fetchUpcomingMovies = async () => {
+      try {
+        const response = await fetch(
+          'https://api.themoviedb.org/3/movie/upcoming?api_key=15979629ea6e558ef491c9b9ccee0043',
+        );
+        const data = await response.json();
+        setUpcomingMovies(data.results);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUpcomingMovies();
   }, []);
 
   // Handle adding/removing movies to/from lists
@@ -67,6 +87,8 @@ function MoviesScreen() {
       }
       return [...prevList, movie];
     });
+    // Remove from My List if present
+    setMyList(prevList => prevList.filter(item => item.id !== movie.id));
   };
 
   const handleShowOptions = (movie, type) => {
@@ -79,6 +101,38 @@ function MoviesScreen() {
     setModalVisible(false);
     setSelectedMovie(null);
     setListType(null);
+  };
+
+  // Handle modal option selection
+  const handleOptionSelect = option => {
+    if (!selectedMovie) {
+      return;
+    }
+
+    switch (option) {
+      case 'myList':
+        handleAddToMyList(selectedMovie);
+        break;
+      case 'like':
+        handleAddToLiked(selectedMovie);
+        break;
+      case 'watched':
+        handleAddToWatched(selectedMovie);
+        break;
+      case 'remove':
+        if (listType === 'myList') {
+          handleAddToMyList(selectedMovie);
+        } else if (listType === 'likedList') {
+          handleAddToLiked(selectedMovie);
+        } else if (listType === 'watchedList') {
+          handleAddToWatched(selectedMovie);
+        }
+        break;
+      default:
+        break;
+    }
+
+    handleCloseModal();
   };
 
   // Render content based on state
@@ -104,7 +158,31 @@ function MoviesScreen() {
       <View style={styles.sectionContainer}>
         <Text style={styles.sectionTitle}>Popular Recommendations</Text>
         <FlatList
-          data={movies}
+          data={popularMovies}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) => (
+            <View style={styles.movieContainer}>
+              <TouchableOpacity
+                onPress={() => handleShowOptions(item, 'recommendations')}>
+                <Image
+                  style={styles.poster}
+                  source={{
+                    uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                  }}
+                />
+                <Text style={styles.title}>{item.title}</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+          horizontal
+        />
+      </View>
+
+      {/* Upcoming Recommendations Section */}
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Upcoming Recommendations</Text>
+        <FlatList
+          data={upcomingMovies}
           keyExtractor={item => item.id.toString()}
           renderItem={({item}) => (
             <View style={styles.movieContainer}>
@@ -152,19 +230,19 @@ function MoviesScreen() {
         )}
       </View>
 
-      {/* Liked Section */}
+      {/* Watched Section */}
       <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Liked</Text>
-        {likedList.length === 0 ? (
+        <Text style={styles.sectionTitle}>Watched</Text>
+        {watchedList.length === 0 ? (
           <Text style={styles.text}>No movies added yet.</Text>
         ) : (
           <FlatList
-            data={likedList}
+            data={watchedList}
             keyExtractor={item => item.id.toString()}
             renderItem={({item}) => (
               <View style={styles.movieContainer}>
                 <TouchableOpacity
-                  onPress={() => handleShowOptions(item, 'likedList')}>
+                  onPress={() => handleShowOptions(item, 'watchedList')}>
                   <Image
                     style={styles.poster}
                     source={{
@@ -180,19 +258,19 @@ function MoviesScreen() {
         )}
       </View>
 
-      {/* Watched Section */}
+      {/* Liked Section */}
       <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Watched</Text>
-        {watchedList.length === 0 ? (
+        <Text style={styles.sectionTitle}>Liked</Text>
+        {likedList.length === 0 ? (
           <Text style={styles.text}>No movies added yet.</Text>
         ) : (
           <FlatList
-            data={watchedList}
+            data={likedList}
             keyExtractor={item => item.id.toString()}
             renderItem={({item}) => (
               <View style={styles.movieContainer}>
                 <TouchableOpacity
-                  onPress={() => handleShowOptions(item, 'watchedList')}>
+                  onPress={() => handleShowOptions(item, 'likedList')}>
                   <Image
                     style={styles.poster}
                     source={{
@@ -222,24 +300,15 @@ function MoviesScreen() {
                 <>
                   <Button
                     title="Add to My List"
-                    onPress={() => {
-                      handleAddToMyList(selectedMovie);
-                      handleCloseModal();
-                    }}
-                  />
-                  <Button
-                    title="Like"
-                    onPress={() => {
-                      handleAddToLiked(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    onPress={() => handleOptionSelect('myList')}
                   />
                   <Button
                     title="Mark as Watched"
-                    onPress={() => {
-                      handleAddToWatched(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    onPress={() => handleOptionSelect('watched')}
+                  />
+                  <Button
+                    title="Mark as Liked"
+                    onPress={() => handleOptionSelect('like')}
                   />
                 </>
               )}
@@ -247,24 +316,15 @@ function MoviesScreen() {
                 <>
                   <Button
                     title="Remove from My List"
-                    onPress={() => {
-                      handleAddToMyList(selectedMovie);
-                      handleCloseModal();
-                    }}
-                  />
-                  <Button
-                    title="Like"
-                    onPress={() => {
-                      handleAddToLiked(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    onPress={() => handleOptionSelect('remove')}
                   />
                   <Button
                     title="Mark as Watched"
-                    onPress={() => {
-                      handleAddToWatched(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    onPress={() => handleOptionSelect('watched')}
+                  />
+                  <Button
+                    title="Mark as Liked"
+                    onPress={() => handleOptionSelect('like')}
                   />
                 </>
               )}
@@ -272,24 +332,11 @@ function MoviesScreen() {
                 <>
                   <Button
                     title="Remove from Liked List"
-                    onPress={() => {
-                      handleAddToLiked(selectedMovie);
-                      handleCloseModal();
-                    }}
-                  />
-                  <Button
-                    title="Add to My List"
-                    onPress={() => {
-                      handleAddToMyList(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    onPress={() => handleOptionSelect('remove')}
                   />
                   <Button
                     title="Mark as Watched"
-                    onPress={() => {
-                      handleAddToWatched(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    onPress={() => handleOptionSelect('watched')}
                   />
                 </>
               )}
@@ -297,24 +344,15 @@ function MoviesScreen() {
                 <>
                   <Button
                     title="Remove from Watched List"
-                    onPress={() => {
-                      handleAddToWatched(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    onPress={() => handleOptionSelect('remove')}
                   />
                   <Button
-                    title="Add to Liked List"
-                    onPress={() => {
-                      handleAddToLiked(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    title="Mark as Liked"
+                    onPress={() => handleOptionSelect('like')}
                   />
                   <Button
                     title="Add to My List"
-                    onPress={() => {
-                      handleAddToMyList(selectedMovie);
-                      handleCloseModal();
-                    }}
+                    onPress={() => handleOptionSelect('myList')}
                   />
                 </>
               )}
