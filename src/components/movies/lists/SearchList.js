0fe7651275906e981388bbdbd20800f8b05779ac
Justin Kurdila <justinkurdila@gmail.com>
2024-08-12 20:24:32 -0400

    adding autocomplete

--- src/components/movies/lists/SearchList.js
+++ src/components/movies/lists/SearchList.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useCallback} from 'react';
 import {
   View,
   Text,
@@ -8,12 +9,48 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native';
+import debounce from 'lodash.debounce';
 
 const apiKey = '15979629ea6e558ef491c9b9ccee0043';
 
 const SearchList = ({handleShowOptions}) => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const debouncedFetchSuggestions = useCallback(
+    debounce(query => fetchSuggestions(query), 300), // 300ms debounce
+    [],
+  );
+
+  const fetchSuggestions = async query => {
+    if (query.length < 3) {
+      return;
+    } // Minimum length to start fetching
+
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
+          query,
+        )}`,
+      );
+      const data = await response.json();
+      setSuggestions(data.results.slice(0, 5));
+    } catch (error) {
+      console.error('Error fetching movie suggestions:', error);
+    }
+  };
+
+  const handleChangeText = text => {
+    setQuery(text);
+    debouncedFetchSuggestions(text);
+  };
+
+  const handleSuggestionPress = movie => {
+    setQuery(movie.title);
+    setSuggestions([]);
+  };
 
   const searchMovies = async () => {
     try {
@@ -23,7 +60,6 @@ const SearchList = ({handleShowOptions}) => {
         )}`,
       );
       const data = await response.json();
-      console.log(data.results); // Debugging line
       setResults(data.results.slice(0, 3));
     } catch (error) {
       console.error('Error fetching movies:', error);
@@ -33,10 +69,7 @@ const SearchList = ({handleShowOptions}) => {
   const clearSearch = () => {
     setQuery('');
     setResults([]);
-  };
-
-  const handlePress = movie => {
-    handleShowOptions(movie, 'recommendations');
+    setSuggestions([]);
   };
 
   return (
@@ -46,18 +79,39 @@ const SearchList = ({handleShowOptions}) => {
         placeholder="Search for movies..."
         placeholderTextColor="#FBF4F4"
         value={query}
-        onChangeText={setQuery}
+        onChangeText={handleChangeText}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
+      {isFocused && suggestions.length > 0 && (
+        <FlatList
+          style={styles.suggestionsList}
+          data={suggestions}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              style={styles.suggestionItem}
+              onPress={() => handleSuggestionPress(item)}>
+              <Text style={styles.suggestionText}>{item.title}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      )}
       <View style={styles.buttonContainer}>
-        <CustomButton title="Search" onPress={searchMovies} />
-        <CustomButton title="Clear" onPress={clearSearch} />
+        <TouchableOpacity style={styles.button} onPress={searchMovies}>
+          <Text style={styles.buttonText}>Search</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={clearSearch}>
+          <Text style={styles.buttonText}>Clear</Text>
+        </TouchableOpacity>
       </View>
       {results.length > 0 ? (
         <FlatList
           data={results}
           keyExtractor={item => item.id.toString()}
           renderItem={({item}) => (
-            <TouchableOpacity onPress={() => handlePress(item)}>
+            <TouchableOpacity
+              onPress={() => handleShowOptions(item, 'recommendations')}>
               <View style={styles.item}>
                 {item.poster_path && (
                   <Image
@@ -78,12 +132,6 @@ const SearchList = ({handleShowOptions}) => {
     </View>
   );
 };
-
-const CustomButton = ({title, onPress}) => (
-  <TouchableOpacity style={styles.button} onPress={onPress}>
-    <Text style={styles.buttonText}>{title}</Text>
-  </TouchableOpacity>
-);
 
 const styles = StyleSheet.create({
   container: {
@@ -111,6 +159,7 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#595959', // Set the text color for the button
     fontSize: 16,
+    fontWeight: 'bold',
   },
   item: {
     marginBottom: 10,
@@ -131,6 +180,21 @@ const styles = StyleSheet.create({
     color: '#FBF4F4', // Set the text color for "No results found."
     fontSize: 16,
     textAlign: 'center',
+  },
+  suggestionsList: {
+    position: 'absolute',
+    top: 50, // Adjust according to the position of your TextInput
+    left: 10,
+    right: 10,
+    backgroundColor: '#595959',
+    maxHeight: 200, // Set a max height for the suggestions list
+    zIndex: 1,
+  },
+  suggestionItem: {
+    padding: 10,
+  },
+  suggestionText: {
+    color: '#FBF4F4',
   },
 });
 
