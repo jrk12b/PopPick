689d04072423acca5b767f94435e948a42c1f0cf
Justin Kurdila <justinkurdila@gmail.com>
2024-09-01 16:56:05 -0400

    adding simple test

---
+++ __tests__/unit/App.test.ts
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+// Note: import explicitly to use the types shipped with jest.
+import {it} from '@jest/globals';
+
+describe('Simple Test Suite', () => {
+  it('should always pass', () => {
+    expect(true).toBe(true);
+  });
+});
